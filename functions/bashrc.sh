d1d6a9585b157c8acebe21423387d885d5d3d350
Khyber Sen <kkysen@gmail.com>
2020-02-19 12:41:33 -0500

    Added options to `bashrc`.
    `edit` and `code` both open ${BASH_DIR} in code (VSC).
    `cd` and `go` cd into ${BASH_DIR}
    `bashrc` is also aliases as `rc`.

--- functions/bashrc.sh
+++ functions/bashrc.sh
@@ -1,9 +1,16 @@
 bashrc() {
 	if [[ "${1}" == "edit" ]]; then
-		code $BASH_DIR
+		code ${BASH_DIR}
+	elif [[ "${1}" == "code" ]]; then
+		code ${BASH_DIR}
+	elif [[ "${1}" == "cd" ]]; then
+		cd ${BASH_DIR}
+	elif [[ "${1}" == "go" ]]; then
+		cd ${BASH_DIR}
 	else
 		. ~/.bashrc
 	fi
 }
 
 export -f bashrc
+alias rc=bashrc
