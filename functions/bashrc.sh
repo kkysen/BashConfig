5332819430c8b8ca3f32e9f10334331622ec6710
Khyber Sen <kkysen@gmail.com>
2020-02-25 05:02:25 -0500

    `sh` option for `bashrc` now uses `sk` to find the script to reload.

--- functions/bashrc.sh
+++ functions/bashrc.sh
@@ -1,3 +1,18 @@
+reloadScript() {
+	# if given a file in ${FUNCTIONS}, reload only that file
+	local file="${1}"
+	local path="${FUNCTIONS}/${file}"
+	if [[ ! -f "${path}" ]]; then
+		echo "\`${path}\` does not exist"
+		return 1
+	fi
+	if [[ ! "${path}" == *.sh ]]; then
+		echo "\`${path}\` is not a bash script ending in .sh"
+		return 1
+	fi
+	. "${path}"
+}
+
 bashrc() {
 	local arg="${1}"
 	case "${arg}" in
@@ -10,28 +25,22 @@ bashrc() {
 		"")
 			. ~/.bashrc
 			;;
+		"sh")
+			local file=$(cd "${FUNCTIONS}" && SKIM_DEFAULT_COMMAND="rg --files | rg '.sh'" sk)
+			reloadScript "${file}"
+			;;
 		"-h" | "-help" | "--help")
 			echo "Usage: ${FUNCNAME[0]} [edit | code | cd | go | <bash script in ${FUNCTIONS}>]"
 			return 1
 			;;
 		*)
-			# if given a file in ${FUNCTIONS}, reload only that file
-			local path="${FUNCTIONS}/${arg}"
-			if [[ ! -f "${path}" ]]; then
-				echo "\`${path}\` does not exist"
-				return 1
-			fi
-			if [[ ! "${path}" == *.sh ]]; then
-				echo "\`${path}\` is not a bash script ending in .sh"
-				return 1
-			fi
-			. "${path}"
+			reloadScript "${arg}"
 			;;
 	esac
 }
 
 bashrc_complete() {
-	COMPREPLY+=($(compgen -W "edit code cd go -h -help --help" -- "${COMP_WORDS[1]}"))
+	COMPREPLY+=($(compgen -W "edit code cd go sh -h -help --help" -- "${COMP_WORDS[1]}"))
 	local IFS=$'\n'
     COMPREPLY+=($(cd "${FUNCTIONS}" && compgen -f -X '!*.sh' -- "${COMP_WORDS[1]}"))
 }
