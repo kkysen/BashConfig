44d420cb3573768ecdc60d75a487ffa7ef45ded8
Khyber Sen <kkysen@gmail.com>
2020-04-01 19:26:40 -0400

    `last` now uses '\0' as delimiter when given `-0` or `--null`, instead of looking at if the output is a terminal.
    `last` now always prints '.' first no matter what,
    and uses `huniq` to not print redundant '.'.
    
    Deleted `last???` functions, which are the same as `last ???`.

--- functions/last.sh
+++ functions/last.sh
@@ -16,38 +16,36 @@ convertTimeToPrintf() {
     esac
 }
 
+lastRaw() {
+    local timeFormat="${1}"
+    printf ".\0"
+    find "${@:2}" -printf "%${timeFormat}s\t%p\0" \
+        | sort -nrz \
+        | cut -f2 -z
+}
+
 last() {
+    local null="${1}"
+    case "${null}" in
+        --null | -0)
+            local delim='\0'
+            shift
+            ;;
+        *)
+            local delim='\n'
+            ;;
+    esac
     local timeType="${1}"
-    local dir="${2-.}"
     local timeFormat
     if ! timeFormat=$(convertTimeToPrintf "${timeType}"); then
         echo >&2 "timeType must be one of [${timeFormat}]"
         return 1
     fi
-    if [[ -t 1 ]]; then
-        local delim='\n'
-    else
-        local delim='\0'
-    fi
-    find "${dir}" "${@:3}" -printf "%${timeFormat}s\t%p\0" \
-        | sort -nrz \
-        | cut -f2 -z \
+
+    # huniq removes duplicate "."
+    lastRaw "${timeFormat}" "${@:2}" \
+        | huniq -0 \
         | tr '\0' "${delim}"
 }
 
-lastCreated() {
-    last created "${@}"
-}
-
-lastModified() {
-    last modified "${@}"
-}
-
-lastAccessed() {
-    last accessed "${@}"
-}
-
 export -f last
-export -f lastCreated
-export -f lastModified
-export -f lastAccessed
