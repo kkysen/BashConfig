f9ed7062542a6721ac7060c60194d72b59a896fa
Khyber Sen <kkysen@gmail.com>
2020-04-04 18:20:48 -0400

    Inlined `~/.fzf.bash` into `fzf.sh`.

--- config/fzf.sh
+++ config/fzf.sh
@@ -1,1 +1,19 @@
-. "${HOME}/.fzf.bash"
+# Setup fzf
+# ---------
+FZF_HOME="${HOMEBREW_PREFIX}/opt/fzf"
+FZF_BIN="${FZF_HOME}/bin"
+FZF_SHELL="${FZF_HOME}/shell"
+
+if [[ ! "${PATH}" == *"${FZF_BIN}"* ]]; then
+    export PATH="${PATH}:${FZF_BIN}"
+fi
+
+# Auto-completion
+# ---------------
+if [[ $- == *i* ]]; then
+    . "${FZF_SHELL}/completion.bash" 2>/dev/null
+fi
+
+# Key bindings
+# ------------
+. "${FZF_SHELL}/key-bindings.bash"
