e1a978d0daa2b1fd835b2e6e6b4b142ea1d07704
Khyber Sen <kkysen@gmail.com>
2020-04-05 02:13:01 -0400

    `installLocal` now searches $PATH for the target if it's not found locally. If the target is in $WORKSPACE_BIN already, though, it doesn't.

--- functions/installLocal.sh
+++ functions/installLocal.sh
@@ -1,5 +1,16 @@
 installLocal() {
-    local path="${1}"
+    local rawPath="${1}"
+    local path="${rawPath}"
+    if [[ ! -x "${rawPath}" ]]; then
+        if ! path=$(command -v "${rawPath}"); then
+            echo >&2 "${rawPath} is not an executable or in \$PATH"
+            return 1
+        fi
+        if [[ "${path}" == "${WORKSPACE_BIN}/${rawPath}" ]]; then
+            echo >&2 "${rawPath} is already in ${WORKSPACE_BIN}"
+            return 1
+        fi
+    fi
     ln -s "$(realpath "${path}")" "${WORKSPACE_BIN}"
 }
 
