c52b8a43753ec9a3c3177c857b0f7ba0c1771077
Khyber Sen <kkysen@gmail.com>
2020-04-05 02:23:04 -0400

    Split `pyenv.sh` into `pyenv.generate.sh` and `pyenv.virtualenv.generate.sh`, too. Caching these generated files greatly reduces the time of `rc` from ~4 to ~1 secs, since `pyenv` and Python are really slow.

--- config/pyenv.sh
+++ config/pyenv.sh
@@ -1,8 +1,1 @@
 export PYENV_ROOT="${WORKSPACE}/Python/pyenv"
-
-if command -v pyenv 1>/dev/null 2>&1; then
-    eval "$(pyenv init -)"
-    if [[ -d "$(pyenv root)/pyenv-virtualenv" ]]; then
-        eval "$(pyenv virtualenv-init -)"
-    fi
-fi
