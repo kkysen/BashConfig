f60f86bed8a26157b3157406b6789491298dfef7
Khyber Sen <kkysen@gmail.com>
2020-04-04 23:55:29 -0400

    Reworked `arbtt`.  `arbttRaw` calls the raw `arbtt-${subcommand}.exe` win32 program.
    This also allowed me to remove arbtt from the Windows system PATH.

--- functions/arbtt.sh
+++ functions/arbtt.sh
@@ -1,7 +1,30 @@
 ARBTT_DIR="${APP_DATA}/Roaming/arbtt"
 
+ARBTT_BIN="${PROGRAM_FILES_X86}/arbtt/bin"
+
+arbttRaw() {
+    local command="${1}"
+    local path="${ARBTT_BIN}/arbtt-${command}.exe"
+    "${path}" "${@:2}"
+}
+
+export -f arbttRaw
+
+arbttRaw_compgen() {
+    fd --base-directory "${ARBTT_BIN}" "arbtt" \
+        | sd "arbtt-(.*)\.exe" '$1'
+}
+
+arbttRaw_complete() {
+    compReply arbttRaw_compgen
+}
+
+complete -F arbttRaw_complete arbttRaw
+
 arbtt() {
-    arbtt-stats --logfile="${ARBTT_DIR}/capture.log" --categorizefile="${ARBTT_DIR}/categorize.cfg" "${@}"
+#    arbttRaw stats --logfile="${ARBTT_DIR}/capture.log" --categorizefile="${ARBTT_DIR}/categorize.cfg" "${@}"
+    # TODO format this better w/ categorize.cfg if I want better stats
+    arbttRaw stats "${@}"
 }
 
 export -f arbtt
