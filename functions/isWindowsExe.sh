8486906abde775d35ba736dbd43efb22429e8512
Khyber Sen <kkysen@gmail.com>
2020-05-03 01:51:46 -0400

    `isWindowsExe` now works on executables in $PATH, too.

--- functions/isWindowsExe.sh
+++ functions/isWindowsExe.sh
@@ -1,5 +1,8 @@
 isWindowsExe() {
     local executable="${1}"
+    if [[ ! -e "${executable}" ]]; then
+        executable=$(command -v "${executable}")
+    fi
     local fileType=$(file "${executable}")
     if [[ "${fileType}" == *"Windows"* ]] || [[ "${fileType}" == *"DOS"* ]]; then
         return 0
