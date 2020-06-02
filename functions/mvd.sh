e0dc9830b7ecda62926a59313a0266cfca155c2f
Khyber Sen <kkysen@gmail.com>
2020-06-02 16:29:37 -0400

    `mvd` w/o args now runs `downloads mvTo .`.

--- functions/mvd.sh
+++ functions/mvd.sh
@@ -7,9 +7,12 @@ mvd() {
     # shellcheck disable=SC2153
     local downloads="${DOWNLOADS}"
     if [[ "$#" -eq 0 ]]; then
-        echo "Usage: ${FUNCNAME[0]} [destDir] [files in ${downloads}]"
-        return 1
+        downloads mvTo .
     elif [[ "$#" -eq 1 ]]; then
+        if [[ "${1}" == "--help" ]]; then
+            echo "Usage: ${FUNCNAME[0]} [destDir] [files in ${downloads}]" 1>&2
+            return 1
+        fi
         local dest="."
         local download="${1}"
         mvEcho "${downloads}/${download}" "${dest}"
