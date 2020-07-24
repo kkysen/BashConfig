f9c72f7246335431279dc7e2cc4cbd243332370b
Khyber Sen <kkysen@gmail.com>
2020-07-24 14:44:09 -0400

    Factored `selectLine` out from `pselect`, so it's easier to select a line w/ `skim`.

--- functions/pselect.sh
+++ functions/pselect.sh
@@ -1,7 +1,15 @@
+selectLine() {
+    if [[ ${#} -eq 0 ]]; then
+        skim --tac --header-lines 1
+    else
+        rg "${@}"
+    fi
+}
+
 pselect() {
     ps -e --format pid,ppid,pcpu,pmem,etime,user,comm,args |
         awk '$5 != "00:00"' |
-        skim --tac --header-lines 1 |
+        selectLine "${@}" |
         awk '{print $1}'
 }
 
