3d84ffadb12a54f33dee3a34480b8560f6137633
Khyber Sen <kkysen@gmail.com>
2020-07-24 14:44:49 -0400

    Added `purgeHistory`, which deletes matching lines from history, and first checks to confirm if you want those lines deleted.

--- functions/history.sh
+++ functions/history.sh
@@ -25,3 +25,33 @@ export HISTFILE="${DATA_DIR}/bash_history.txt"
 
 alias ignore=""
 export HISTIGNORE="ignore *"
+
+historyToPurge() {
+    history | "${@}"
+    history 1 # purge the purging command
+}
+
+doPurgeHistory() {
+    # uniq to delete potential repeat of last command
+    # tac to reverse indices so that indices aren't moved after removing each one
+    map history -d < <(historyToPurge "${@}" | awk '{print $1}' | uniq | tac)
+    history -w
+}
+
+purgeHistory() {
+    echo "Purging history that matches '${*}'"
+    echo
+    historyToPurge "${@}"
+    echo
+    echo "Can I purge these commands? [y/N]"
+    read -r confirmation
+    if [[ "${confirmation}" =~ ^(yes|y)$ ]]; then
+        doPurgeHistory "${@}"
+        echo "Purged"
+    else
+        echo "Cancelled"
+    fi
+}
+
+export -f historyToPurge
+export -f purgeHistory
