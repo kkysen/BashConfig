3d7998d63abd1485956493ae17c7934654b858cf
Khyber Sen <kkysen@gmail.com>
2020-11-01 03:07:14 -0500

    Added `volta`, an even better `fnm`, which was already great.

--- imports/imports.sh
+++ imports/imports.sh
@@ -1,5 +1,6 @@
 export PATH="${WORKSPACE}/bin:${PATH}" # fnm on path
 . "${CONFIGS}/fnm.sh"                  # correct node version on path
+. "${CONFIGS}/volta.sh"
 . "${FUNCTIONS}/node.sh"               # node_mjs for import
 . "${FUNCTIONS}/import.sh"             # import func
 . "${FUNCTIONS}/imports.sh"            # importAll func
