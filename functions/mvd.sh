3e305fc6ab26afdeba1475467d8b1ca8c5391452
Khyber Sen <kkysen@gmail.com>
2021-01-02 11:49:51 -0500

    Updated `mvd` to use `downloads mvTo .`, which has a much nicer UI.

--- functions/mvd.sh
+++ functions/mvd.sh
@@ -4,39 +4,8 @@ mvEcho() {
 }
 
 mvd() {
-    # shellcheck disable=SC2153
-    local downloads="${DOWNLOADS}"
-    if [[ "$#" -eq 0 ]]; then
-        downloads mvTo .
-    elif [[ "$#" -eq 1 ]]; then
-        if [[ "${1}" == "--help" ]]; then
-            echo "Usage: ${FUNCNAME[0]} [destDir] [files in ${downloads}]" 1>&2
-            return 1
-        fi
-        local dest="."
-        local download="${1}"
-        mvEcho "${downloads}/${download}" "${dest}"
-    else
-        local dest="${1}"
-        for download in "${@:2}"; do
-            mvEcho "${downloads}/${download}" "${dest}"
-        done
-    fi
+    local dir="${1-.}"
+    downloads mvTo "${dir}"
 }
 
 export -f mvd
-
-mvd_compgen() {
-    local i="${1}"
-    local arg="${2}"
-    if [[ ${i} -ne 1 ]]; then
-        return
-    fi
-    cd "${DOWNLOADS}" && compgen -fd -- "${arg}"
-}
-
-mvd_complete() {
-    compReply mvd_compgen
-}
-
-complete -F mvd_complete mvd
