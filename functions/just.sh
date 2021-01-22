f288b2792db51eab53803c9d7bb7f5fa37a23009
Khyber Sen <kkysen@gmail.com>
2021-01-22 08:34:15 -0500

    Improve `just` completion a bit. Now if the previous argument was an --option, the recipe completions extends.

--- functions/just.sh
+++ functions/just.sh
@@ -7,7 +7,9 @@ export -f j
 just_compgen() {
     local i="${1}"
     local arg="${2}"
-    if [[ ${i} -eq 1 ]]; then
+    local j=$((i - 1))
+    local prev="${COMP_WORDS[${j}]}"
+    if [[ ${i} -eq 1 || "${prev}" == --* ]]; then
         compgen -W "$(just --summary)" -- "${arg}"
         compgen -W "$(just --list | rg alias | awk '{print $1}')" -- "${arg}"
     else
